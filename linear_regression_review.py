289a4bdc5a471eacd2b13e7310a94b172ab51707
Enzo Souza <enzo.florentinosouza.ismart@gmail.com>
2025-03-05 17:06:44 -0300

    revisao de linear regression

---
+++ linear_regression_review.py
@@ -0,0 +1,10 @@
+#regression problem in machine learning: trying to predict a target numeric value
+#linear regression: most basic kind of regression
+#linear regression models use linear equation to make predictions y = mx + b
+#rent = 2.5sz_sqft + 1000
+#rent: output or target
+#sz_sqft: input_feature
+#2.5: weight associated to sz_sqft
+#1000: bias
+#More than one input feature:
+#rent = 2.5sz_sqft - 1.5age + 1000
